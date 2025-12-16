723d07bafe5322c3f3be9657f176f8dd49d5d635
snihaal2006 <snihaal2006@gmail.com>
2025-12-16 10:25:46 +0530

    Small Change\

--- React-project/src/component/Login.jsx
+++ React-project/src/component/Login.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 function Login() {
+
+  let  data= true;
   return (
     <div>
-      <h2>Login Component</h2>
+      <h2>{data?'You are onlineeeee :)':'You r Offline:('}</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ipsam inventore maxime facilis ipsa. Suscipit, at cupiditate nisi delectus corporis, harum ipsa esse blanditiis labore placeat commodi. Assumenda, nesciunt quibusdam?</p>
     </div>
   );
